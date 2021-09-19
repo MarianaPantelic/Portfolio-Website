bfa779bfa66d8f69bb3f5d62b37cb25767e4bfd6
Mariana Pantelic <mari27line@yahoo.com>
2021-09-19 18:19:26 +0200

    Added the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import { Nav } from "react-bootstrap/";
+import logo from "../images/MP-logo-transparent-bg.png";
 
-const Navbar = () => {
-  return <div></div>;
+const Navigation = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">
+        <img src={logo} className="logo" alt="Mariana Pantelic Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/Projects">Portfolio</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
-export default Navbar;
+export default Navigation;
